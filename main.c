9dd2055806ab8bc041ab0e2a0579971aa783be4e
minseo0520 <141472390+minseo0520@users.noreply.github.com>
2023-09-14 16:34:52 +0900

    실습1수정

--- main.c
+++ main.c
@@ -4,6 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char*argv[]){
-	printf("юс╨С гоюл");
+	printf("hello");
 	return 0; 
 }
